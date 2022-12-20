6ea2ed1909365ba061e0502a38c2189f5a0c2815
NamanShah12345 <naman@technoproject.in>
2022-12-20 18:51:43 +0530

    chat app

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+let users = [];
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+
+    socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+
+    socket.on('newUser', (data) => {
+        //Adds the new user to the list of users
+        users.push(data);
+        // console.log(users);
+        //Sends the list of users to the client
+        socketIO.emit('newUserResponse', users);
+    });
+
+    socket.on('message', (data) => {
+        socketIO.emit('messageResponse', data);
+    });
+    socket.on('disconnect', () => {
+        console.log('🔥: A user disconnected');
+        users = users.filter((user) => user.socketID !== socket.id);
+        socketIO.emit('newUserResponse', users);
+        socket.disconnect();
+    });
+});
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Hello world',
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
